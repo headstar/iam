c28c577999d5894c82364c03d841f569149d1ada
Per Johansson <per.johansson@post.com>
2016-02-24 21:45:27 +0000

    fixed @TransactionalService

--- iam-core/src/main/java/com/headstartech/iam/core/annotations/TransactionalService.java
+++ iam-core/src/main/java/com/headstartech/iam/core/annotations/TransactionalService.java
@@ -3,7 +3,17 @@ package com.headstartech.iam.core.annotations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
 @Service
 @Transactional(rollbackFor = { Exception.class })
+@Target(TYPE)
+@Retention(RUNTIME)
+@Documented
 public @interface TransactionalService {
 }
