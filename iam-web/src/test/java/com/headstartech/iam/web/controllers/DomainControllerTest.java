f505346d7b28cca162d02b489fcc4d1435f6d052
Per Johansson <per.johansson@post.com>
2016-03-08 21:45:45 +0000

    more test work

--- iam-web/src/test/java/com/headstartech/iam/web/controllers/DomainControllerTest.java
+++ iam-web/src/test/java/com/headstartech/iam/web/controllers/DomainControllerTest.java
@@ -1,24 +1,32 @@
 package com.headstartech.iam.web.controllers;
 
 import com.headstartech.iam.IAMWeb;
+import com.headstartech.iam.core.jpa.repositories.JpaDomainRepository;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles({"integration"})
 @SpringApplicationConfiguration(classes = IAMWeb.class)
 @WebIntegrationTest(randomPort = true)
 public class DomainControllerTest {
 
-    // Since we're bringing the service up on random port need to figure out what it is
     @Value("${local.server.port}")
     private int port;
 
+    @Autowired
+    private JpaDomainRepository jpaDomainRepository;
+
     @Test
     public void foo() {
-
+        Assert.assertThat(this.jpaDomainRepository.count(), Matchers.is(0L));
     }
 }
