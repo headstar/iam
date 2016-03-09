7f4d3fc359d03562549cb415f0ccc2b2177647d7
Per Johansson <per.johansson@post.com>
2016-03-09 21:41:29 +0000

    more tests

--- iam-web/src/test/java/com/headstartech/iam/web/controllers/DomainControllerTest.java
+++ iam-web/src/test/java/com/headstartech/iam/web/controllers/DomainControllerTest.java
@@ -30,6 +30,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.UUID;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles({"integration"})
 @SpringApplicationConfiguration(classes = IAMWeb.class)
@@ -60,7 +62,6 @@ public class DomainControllerTest {
 
     @Test
     public void canCreateWithoutId() {
-        // given
         Domain request = new Domain();
         request.setDescription("abc");
 
@@ -81,4 +82,87 @@ public class DomainControllerTest {
         assertEquals(1, domainCountAfter - domainCountBefore);
         assertTrue(jpaDomainRepository.exists(response.getId()));
     }
+
+    @Test
+    public void canCreateWithId() {
+        Domain request = new Domain();
+        request.setId(UUID.randomUUID().toString());
+        request.setDescription("abc");
+
+        long domainCountBefore = jpaDomainRepository.count();
+
+        Domain response = given()
+                .accept(MediaTypes.HAL_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .post("/api/domains")
+                .then()
+                .statusCode(201).extract().as(Domain.class);
+
+        long domainCountAfter = jpaDomainRepository.count();
+
+        assertFalse(response.getId().isEmpty());
+        assertEquals(request.getDescription(), response.getDescription());
+        assertEquals(1, domainCountAfter - domainCountBefore);
+        assertTrue(jpaDomainRepository.exists(request.getId()));
+    }
+
+    @Test
+    public void canGetExisting() {
+        Domain domain = createDomain(UUID.randomUUID().toString());
+
+        Domain response = given()
+                .accept(MediaTypes.HAL_JSON_VALUE)
+                .get("/api/domains/{id}", domain.getId())
+                .then()
+                .statusCode(200).extract().as(Domain.class);
+
+        assertEquals(domain.getId(), response.getId());
+        assertEquals(domain.getDescription(), response.getDescription());
+    }
+
+    @Test
+    public void canUpdateExisting() {
+        Domain domain = createDomain(UUID.randomUUID().toString());
+
+        domain.setDescription(domain.getDescription() + "a");
+
+        Domain response = given()
+                .accept(MediaTypes.HAL_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(domain)
+                .put("/api/domains/{id}", domain.getId())
+                .then()
+                .statusCode(200).extract().as(Domain.class);
+
+        assertEquals(domain.getId(), response.getId());
+        assertEquals(domain.getDescription(), response.getDescription());
+    }
+
+    @Test
+    public void canDeleteExisting() {
+        Domain domain = createDomain(UUID.randomUUID().toString());
+        assertTrue(jpaDomainRepository.exists(domain.getId()));
+
+        given()
+                .delete("/api/domains/{id}", domain.getId())
+                .then()
+                .statusCode(204).extract();
+
+        assertFalse(jpaDomainRepository.exists(domain.getId()));
+    }
+
+    Domain createDomain(String id) {
+        Domain request = new Domain();
+        request.setId(id);
+        request.setDescription("desc");
+        return given()
+                .accept(MediaTypes.HAL_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .post("/api/domains")
+                .then()
+                .statusCode(201).extract().as(Domain.class);
+    }
+
 }
