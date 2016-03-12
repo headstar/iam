f3ba4b781e6875bad932eb4e6f104b192cc4fbb1
Per Johansson <per.johansson@post.com>
2016-03-12 19:26:13 +0000

    log validation errors

--- iam-web/src/test/java/com/headstartech/iam/web/controllers/ControllerTestBase.java
+++ iam-web/src/test/java/com/headstartech/iam/web/controllers/ControllerTestBase.java
@@ -54,6 +54,7 @@ public class ControllerTestBase {
     @Before
     public void baseBeforeTestSetup() {
         RestAssured.port = port;
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         domain = createDomain();
     }
 
